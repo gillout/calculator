ec9a783b585ab705ba48de84c97fd5e1a13fe89a
gillout <gillout@bbox.fr>
2020-10-19 23:03:25 +0200

    Feature : Gestion du cas de la division par zéro
    * Correction PR

--- app/model/CalculatorManager.php
+++ app/model/CalculatorManager.php
@@ -137,12 +137,12 @@ class CalculatorManager
                 break;
             case Calculator::DIVIDE:
                 if ($secondOperand == 0) {
-                    throw $e = new Exception('Division par zéro impossible');
+                    throw new Exception('Division par zéro impossible');
                 }
                 $result = $firstOperand / $secondOperand;
                 break;
             default:
-                throw $e = new Exception('La fonction n\'est pas encore implémentée');
+                throw new Exception('La fonction n\'est pas encore implémentée');
         }
         $this->calc->setInput($this->getInput() . $this->calc->getOperator() . $this->calc->getAccumulator());
         $this->calc->setAccumulator(round($result, 8));
